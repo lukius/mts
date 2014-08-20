a314560e147e0859ae3dc6206f9f8b84761f22d5
Lucio Santi <lukius@gmail.com>
2014-08-20 01:05:15 -0300

    S2C5: ECB cut-and-paste.

---
+++ set2/challenge5.py
@@ -0,0 +1,107 @@
+import random
+
+from common.challenge import MatasanoChallenge
+from common.ciphers.block.cipher import AES
+from common.ciphers.block.modes import ECB
+from common.padders import PKCS7Padder
+from common.tools import RandomByteGenerator
+
+
+class UserProfileParser(object):
+
+    FIELDS = ['email', 'uid', 'role']
+
+    def __init__(self, key):
+        self.cipher = AES(key)
+
+    def _parse(self, profile):
+        tuples = profile.split('&')
+        return dict(map(lambda item: item.split('='), tuples))
+    
+    def _profile_has_valid_fields(self, profile):
+        return set(profile.keys()) == set(self.FIELDS)
+    
+    def _validate(self, profile):
+        if not self._profile_has_valid_fields(profile):
+            raise RuntimeError('invalid profile!')
+
+    def parse(self, encrypted_profile):
+        profile_string = self.cipher.decrypt(encrypted_profile, mode=ECB())
+        profile = self._parse(profile_string)
+        self._validate(profile)
+        return profile
+
+
+class UserProfileGenerator(object):
+
+    def __init__(self, key):
+        self.cipher = AES(key)
+        self.profile_template = 'email=%s&uid=%d&role=user'
+
+    def _rand_uid(self):
+        return random.randint(10,99)
+
+    def profile_for(self, email):
+        if '&' in email or '=' in email:
+            raise RuntimeError('email address has invalid characters!')
+        uid = self._rand_uid()
+        profile = self.profile_template % (email, uid)
+        return self.cipher.encrypt(profile, mode=ECB())
+
+
+class AdminUserProfileGenerator(object):
+
+    BLOCK_SIZE = 16
+
+    def __init__(self, profile_generator):
+        self.profile_generator = profile_generator
+        
+    def _get_block(self, index, ciphertext):
+        start_index = index*self.BLOCK_SIZE
+        end_index = start_index+self.BLOCK_SIZE
+        return ciphertext[start_index:end_index]
+        
+    def _merge_blocks(self, ciphertext1, ciphertext2):
+        block1 = self._get_block(0, ciphertext1)
+        block2 = self._get_block(1, ciphertext1)
+        block3 = self._get_block(1, ciphertext2)
+        return '%s%s%s' % (block1, block2, block3)
+
+    def value(self):
+        # 1. Use as input any email that makes the second block end just
+        # before the role:
+        # email=xxxxxx@xx.
+        # com&uid=xx&role=
+        # user
+        email = 'xxxxxx@xx.com'
+        ciphertext1 = self.profile_generator.profile_for(email)
+        
+        # 2. Use as input an email containing 'admin' appropriately padded:
+        # email=xxxxxx@xx.
+        # admin$$$$$$$$$$$
+        # &uid=xx&role=use
+        # r
+        admin_padded = PKCS7Padder('admin').value(self.BLOCK_SIZE) 
+        email = 'xxxxxx@xx.%s' % admin_padded
+        ciphertext2 = self.profile_generator.profile_for(email)
+        
+        # 3. Keep first two blocks from ciphertext1 and second block from
+        # ciphertext2.
+        return self._merge_blocks(ciphertext1, ciphertext2)
+
+
+class Set2Challenge5(MatasanoChallenge):
+
+    BLOCK_SIZE = AdminUserProfileGenerator.BLOCK_SIZE
+
+    def expected_value(self):
+        return 'admin'
+
+    def value(self):
+        secret_key = RandomByteGenerator().value(self.BLOCK_SIZE)
+        profile_generator = UserProfileGenerator(secret_key)
+        # The secret key is not known by the admin profile generator.
+        encrypted_profile = AdminUserProfileGenerator(profile_generator).\
+                            value()
+        profile = UserProfileParser(secret_key).parse(encrypted_profile)
+        return profile['role']
