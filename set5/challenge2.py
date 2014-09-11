1c091eaeff6e35664e950cbab0895d7be6fe00ac
Lucio Santi <lukius@gmail.com>
2014-09-11 01:36:04 -0300

    S5C2: implement a MITM key-fixing attack on Diffie-Hellman with parameter injection.

--- set5/challenge2.py
+++ set5/challenge2.py
@@ -17,6 +17,19 @@ class KeyExchangeProtocolMITMAttack(KeyExchangeProtocol):
         self._init_server_socket()
         self.start()
         
+    def _init_server_socket(self):
+        self.server_socket = socket.socket()
+        self.server_socket.bind((self.SERVER_ADDRESS, self.SERVER_PORT))
+        self.server_socket.listen(1)
+        KeyExchangeProtocolServer.PORT += 1
+
+    def get_message(self):
+        return self.message
+
+    def _wait_for_client_and_connect(self):
+        self.server_socket, _ = self.server_socket.accept()
+        self.client_socket.connect((self.SERVER_ADDRESS, self.SERVER_PORT+1))
+
     def _receive_parameters_from_server(self):
         g = self._receive_int(socket=self.client_socket)
         p = self._receive_int(socket=self.client_socket)
@@ -29,37 +42,27 @@ class KeyExchangeProtocolMITMAttack(KeyExchangeProtocol):
         self._send(p, socket=self.server_socket)
         self._send(p, socket=self.server_socket)         
         
-    def get_message(self):
-        return self.message
-        
-    def _init_server_socket(self):
-        self.server_socket = socket.socket()
-        self.server_socket.bind((self.SERVER_ADDRESS, self.SERVER_PORT))
-        self.server_socket.listen(1)
-        KeyExchangeProtocolServer.PORT += 1
-        
-    def _wait_for_client_and_connect(self):
-        self.server_socket, _ = self.server_socket.accept()
-        self.client_socket.connect((self.SERVER_ADDRESS, self.SERVER_PORT+1))
-        
-    def _decrypt_message(self, iv_and_message, secret):
-        # Secret is fixed to g since g**p = g mod p (Fermat's little theorem).
+    def _decrypt_message(self, iv_and_message):
         iv = iv_and_message[:self.BLOCK_SIZE]
-        self._init_cipher_from(secret, iv)
+        # Secret is fixed to 0 since p**n = 0 mod p for any n.
+        self._init_cipher_from(0, iv)
         message = iv_and_message[self.BLOCK_SIZE:]
-        return self.cipher.decrypt(message, mode=self.cipher_mode).bytes()        
+        return self.cipher.decrypt(message, mode=self.cipher_mode).bytes()    
         
     def _run(self):
         self._wait_for_client_and_connect()
+        # Receive p, g and A from server; ignore A.
         p, g, _ = self._receive_parameters_from_server()
+        # Inject our parameters to the other party.
         self._inject_parameters_to_client(g, p)
+        # Receive B from client and ignore it.
         self._receive_int(socket=self.server_socket)
-        # Inject custom B to server
+        # Inject custom B to server.
         self._send(p, socket=self.client_socket)
         # Receive and relay encrypted message + IV (from server)
         iv_and_message = self._receive(socket=self.client_socket)
         self._send(iv_and_message, socket=self.server_socket)
-        self.message = self._decrypt_message(iv_and_message, g)
+        self.message = self._decrypt_message(iv_and_message)
         # Receive and relay encrypted message + IV (from client)
         iv_and_message = self._receive(socket=self.server_socket)
         self._send(iv_and_message, socket=self.client_socket)        
